8667b748120f6cccba7b635488838d32d86df669
Melissa Real <melissa.real@hotmail.com>
2019-05-27 19:22:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 function init() {
   //put any page initialization/handlebars initialization here
   let firstForm = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
-
   document.getElementById('main').innerHTML = firstForm({ingredients: ['','','','','']});
-
+  
   Handlebars.registerPartial('rec-details', document.getElementById("recipe-details-partial").innerHTML);
 }
 
