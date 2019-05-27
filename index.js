fd9001d34027a5bd094a87a74e6ce67781e21ce8
Melissa Real <melissa.real@hotmail.com>
2019-05-27 19:01:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 function createRecipe(){
   var template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
 
-  document.getElementsByTagName("main")[0].innerHTML += template;
+  document.getElementsByTagName("recipe-form")[0].innerHTML += template;
 }
 
 
